467834b4e9bab6ab44fd44741b39857c33ace875
OlafSt2001 <software@stieleke.de>
2025-04-17 12:56:03 +0200

    required-Properties für LegendItem

--- Models/LegendItem.cs
+++ Models/LegendItem.cs
@@ -7,7 +7,7 @@ namespace RKI2.Models
     {
         public double InzidenzMin { get; set; }
         public double InzidenzMax { get; set; }
-        public string InzidenzRangeText { get; set; }
-        public Brush InzidenzColor { get; set; }
+        public required string InzidenzRangeText { get; set; }
+        public required Brush InzidenzColor { get; set; }
     }
 }
